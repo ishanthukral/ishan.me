e4a283ff7c328952a57f1ac16c28699cb1354a89
Ishan Thukral <dev@ishan.me>
2017-01-03 17:53:28 -0500

    Added about me route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,15 @@
-var routes = require('express').Router();
+var routes  = require('express').Router();
+var request = require('request')
+var dataUrl = 'https://raw.githubusercontent.com/ishanthukral/ishan.me/master/data';
 
 routes.get('/aboutme', function(req, res) {
-  res.status(200).json({ message: 'hello world' });
+  request.get(dataUrl + '/aboutMe.md', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.status(200).json({ message: body });
+    } else {
+      res.status(500).send('Could not find data')
+    }
+  });
 });
 
 module.exports = routes;
