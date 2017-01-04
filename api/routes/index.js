b2b2a4564f1bb902e63dedf6419349e68b222e76
Ishan Thukral <dev@ishan.me>
2017-01-04 11:58:09 -0500

    Added recentPhotos endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,8 @@ var routes  = require('express').Router();
 var request = require('request')
 var apiDataUrl = 'https://raw.githubusercontent.com/ishanthukral/ishan.me/master/data';
 
+/** About Me */
+
 routes.get('/aboutme', function (req, res) {
   request.get(apiDataUrl + '/aboutMe.md', function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -12,6 +14,9 @@ routes.get('/aboutme', function (req, res) {
   });
 });
 
+
+/** Recent Tracks */
+
 routes.get('/recentTracks', function (req, res) {
   request({
     url: 'http://ws.audioscrobbler.com/2.0/',
@@ -19,6 +24,24 @@ routes.get('/recentTracks', function (req, res) {
           user: 'ishanthukral',
           api_key: process.env.LASTFM_APIKEY,
           format: 'json' },
+    method: 'GET'
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.status(200).send(body);
+    } else {
+      res.status(500).send('Could not find data');
+    }
+  });
+});
+
+
+/**  Recent Photos */
+
+routes.get('/recentPhotos', function (req, res) {
+  request({
+    url: 'https://api.instagram.com/v1/users/2114353064/media/recent/',
+    qs: { client_id: process.env.INSTAGRAM_CLIENTID,
+          access_token: process.env.INSTAGRAM_ACCESSTOKEN },
     method: 'GET'
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
