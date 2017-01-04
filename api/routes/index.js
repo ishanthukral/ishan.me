9ab9d81a4681bcbae63f5610d20139f98e09fb57
Ishan Thukral <dev@ishan.me>
2017-01-04 11:51:22 -0500

    Added recent tracks api

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,13 +1,30 @@
 var routes  = require('express').Router();
 var request = require('request')
-var dataUrl = 'https://raw.githubusercontent.com/ishanthukral/ishan.me/master/data';
+var apiDataUrl = 'https://raw.githubusercontent.com/ishanthukral/ishan.me/master/data';
 
-routes.get('/aboutme', function(req, res) {
-  request.get(dataUrl + '/aboutMe.md', function (error, response, body) {
+routes.get('/aboutme', function (req, res) {
+  request.get(apiDataUrl + '/aboutMe.md', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.status(200).json({ message: body });
     } else {
-      res.status(500).send('Could not find data')
+      res.status(500).send('Could not find data');
+    }
+  });
+});
+
+routes.get('/recentTracks', function (req, res) {
+  request({
+    url: 'http://ws.audioscrobbler.com/2.0/',
+    qs: { method: 'user.getrecenttracks',
+          user: 'ishanthukral',
+          api_key: process.env.LASTFM_APIKEY,
+          format: 'json' },
+    method: 'GET'
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.status(200).send(body);
+    } else {
+      res.status(500).send('Could not find data');
     }
   });
 });
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 
 var app     = require('express')();
 var routes  = require('./routes');
+require('dotenv').config()
 
 var port = process.env.PORT || 8080;
 
