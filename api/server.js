3a7e5095bee6e659d1da24f01d051db53fe066d1
Ishan Thukral <dev@ishan.me>
2017-01-05 20:43:24 -0500

    Add cors to api

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@
 
 var app     = require('express')();
 var routes  = require('./routes');
+var cors     = require('cors');
+
 require('dotenv').config()
 
 var port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use('/', routes);
 
 app.listen(port);
